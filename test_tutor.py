7e23f283f2e83b57fe888bdfd8a8a6ac8d75e58f
J. Bobby Lopez <bobby.lopez@gmail.com>
2023-04-16 19:04:11 -0400

    Updated tests to include checks for word generating and floating direction

--- test_tutor.py
+++ test_tutor.py
@@ -1,6 +1,6 @@
 import unittest
 import pygame
-from main import create_floating_letter, create_floating_word, wrap_text
+from main import create_floating_letter, create_floating_word, wrap_text, simple_words, FloatingWord
 
 class TestFloatingLetterCreation(unittest.TestCase):
     def setUp(self):
@@ -33,6 +33,27 @@ class TestTextWrapping(unittest.TestCase):
         text = "This is a long sentence that needs to be wrapped"
         wrapped_text = wrap_text(text, font, 200)
         self.assertIsNotNone(wrapped_text)
+
+class TestWordGeneration(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        pygame.font.init()
+
+    def test_word_in_simple_words(self):
+        font = pygame.font.Font(None, 32)
+        floating_objects = []
+        word = create_floating_word(font, 200, 100, floating_objects)
+        self.assertIn(word.word, simple_words)
+
+class TestFloatingWordBehavior(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        pygame.font.init()
+
+    def test_floating_word_upward_speed(self):
+        font = pygame.font.Font(None, 32)
+        floating_word = FloatingWord("test", font, 100, 100, (0, -2), (255, 255, 255))
+        self.assertEqual(floating_word.speed, (0, -2))
 
 if __name__ == '__main__':
     unittest.main()
